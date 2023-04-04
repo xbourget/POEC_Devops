251c4b6a1f65a0abd0300121b7164ceb3126a609
xbourget <xavier.bourget@gmail.com>
2023-04-04 11:08:10 +0200

    exo split join

---
+++ filename.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8  -*-
+
+print()
+print()
+
+
+chaine = "il fait, beau, mais un peu frais"
+print( chaine )
+
+
+lesMots = chaine.split( ' ' )
+print( lesMots )
+
+lesMots = chaine.split( ',' )
+print( lesMots )
+
+maChaine = "XXXXX".join( lesMots )
+print( maChaine )
+
+maChaine = ",".join( lesMots )
+print( maChaine )
+
+print()
+print( '*' * 50 )
+print()
+
+fileName = "/home/work/work/POEC_DEVOPS_Rennes_Mars/POEC_Devops/faireUneFonction.version2.py"
+fileName = "/home/work/work/POEC_DEVOPS_Rennes_Mars/POEC_Devops/faireUneFonction.version2.html"
+
+fileNameList = fileName.split( '.')
+
+print( fileNameList[ -1] )
+print( fileNameList[ 0 ] + ".copy" )
+
+fileNameList = fileName.split( '/')
+
+print( fileNameList[ -1] )
+
+def changeName( ficName, ext )
+
+ext = "bak"
+fileName = "/home/work/work/POEC_DEVOPS_Rennes_Mars/POEC_Devops/faireUneFonction.version2.py"
+fileName = changeName( fileName, ext  )
+
+#resultat  "/home/work/work/POEC_DEVOPS_Rennes_Mars/POEC_Devops/faireUneFonction.version2.bak"
+
