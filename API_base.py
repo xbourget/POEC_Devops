3ae2c1ea6b650b7ae71368c3a1247292b9a09036
xbourget <xavier.bourget@gmail.com>
2023-04-06 10:08:03 +0200

    api 1

---
+++ API_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8  -*-
+
+import flask
+from flask import request
+import json
+import socket
+
+
+# declaration d'un objet server
+app = flask.Flask(__name__)
+
+nombre_de_roues = {"tram":18, "voiture": 4, "vélo": 2, "tricycle": 3}
+
+#app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def name():
+ dico = { "hostname" : socket.gethostname() }
+ return json.dumps( dico )
+
+@app.route('/vehicule', methods=['GET'])
+def vehicule():
+    nom = request.args.get('nom')
+    return json.dumps( nombre_de_roues[ nom ] )
+
+app.run()
