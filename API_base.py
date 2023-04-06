0632a8f53b7f46ae6ee30060f1abdaaa1662de9a
tanguy35 <kim35@laposte.net>
2023-04-06 13:40:10 +0200

    modif

--- API_base.py
+++ API_base.py
@@ -14,10 +14,13 @@ nombre_de_roues = {"tram":18, "voiture": 4, "vélo": 2, "tricycle": 3}
 
 #app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET'])
+@app.route('/getinfo', methods=['GET'])
 def name():
- dico = { "hostname" : socket.gethostname() }
+ dico = {
+     "hostname" : socket.gethostname(),
+      "ip" : socket.gethostbyname(socket.gethostname()) }
  return json.dumps( dico )
+
 
 @app.route('/vehicule', methods=['GET'])
 def vehicule():
