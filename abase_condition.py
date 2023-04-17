70b134371cecbd78ae723b2bd24b4a3f353299a9
xbourget <xavier.bourget@gmail.com>
2023-04-17 10:20:41 +0200

    aston base

---
+++ abase_condition.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8  -*-
+
+age = 14
+
+if age >= 64 :
+    print( 'retaite')
+    print( 'tu peux conduire une voiture ss permis' )
+elif age >= 18 :
+    print( 'majeur')
+    print( 'tu peux conduire' )
+else :
+    print( 'mineur')
+    print( 'tu peux conduire un vélo' )
+
+print( '*******' )
