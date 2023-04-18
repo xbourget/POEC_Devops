c8f210aa309d1e27d7d56ddd69e4a371a19b663b
IJF444 <IJF444.engie.com>
2023-04-18 15:31:09 +0200

    MAJ Text lines

--- abase_dico_scrabble_vira.py
+++ abase_dico_scrabble_vira.py
@@ -3,9 +3,14 @@ from unidecode import unidecode
 points = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 'I': 1, 'J': 8, 'K': 10, 'L': 1, 'M': 2, 'N': 1, 'O': 1, 'P': 3, 'Q': 8, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 10, 'X': 10, 'Y': 10, 'Z': 10}
 
 text = """
-Si vous donnez aux histoires que vous écrivez à la première personne une vraisemblance telle que 
-les gens finissent par y croire, le lecteur pensera presque forcément qu'elles vous sont effectivement arrivées. Ce qui est tout à fait nature lpuisqu', au moment où vous les inventez , il faut bien que vous donniez l'impression qu'elles sont arrivées à celui qui les raconte. Si votre entreprise est réussie, vous amenez celui qui les lit à croire que ces choses là lui sont également arrivées à lui. Le but que vous vous êtes assigné est atteint, 
-ou peu s'en faut : créer quelque chose susceptible d'imprégner l'expérience et la mémoire de votre lecteur.
+Si vous donnez aux histoires que vous écrivez à la première personne une vraisemblance 
+telle que les gens finissent par y croire, le lecteur pensera presque forcément qu'elles 
+vous sont effectivement arrivées. Ce qui est tout à fait nature puisqu'au moment où vous 
+les inventez , il faut bien que vous donniez l'impression qu'elles sont arrivées à celui 
+qui les raconte. Si votre entreprise est réussie, vous amenez celui qui les lit à croire 
+que ces choses là lui sont également arrivées à lui. Le but que vous vous êtes assigné 
+est atteint, ou peu s'en faut : créer quelque chose susceptible d'imprégner l'expérience 
+et la mémoire de votre lecteur.
 """
 
 text_without_accent = unidecode(text)
