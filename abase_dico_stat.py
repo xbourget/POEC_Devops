3d8749ff0b6d68885d752f870d0b3dccbd5fc22c
Efi67 <fanch_tipost-py@yahoo.com>
2023-04-18 15:19:00 +0200

    maj mardi

--- abase_dico_stat.py
+++ abase_dico_stat.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8  -*-
 
+from abase_dico_scrabble import calculePoints
 
 
-text = """
+
+texte = """
 Si vous donnez aux histoires que vous écrivez à la première personne une vraisemblance telle que 
 les gens finissent par y croire, le lecteur pensera presque forcément qu'elles vous sont effectivement arrivées. Ce qui est tout à fait nature lpuisqu', au moment où vous les inventez , il faut bien que vous donniez l'impression qu'elles sont arrivées à celui qui les raconte. Si votre entreprise est réussie, vous amenez celui qui les lit à croire que ces choses là lui sont également arrivées à lui. Le but que vous vous êtes assigné est atteint, 
 ou peu s'en faut : créer quelque chose susceptible d'imprégner l'expérience et la mémoire de votre lecteur.
 """
 
-mots = text.split( )
+mots = texte.split( )
+
+for mot in mots :
+    try : 
+        print(mot + ' = ' + str(calculePoints(mot)) + ' points')
+    except KeyError :
+        print ( mot + ' : lettre inconnu')
 
 
+"""
 occurence = {}
 
 for mot in mots:
@@ -33,6 +42,8 @@ for mot in mots:
         occurence[ mot ] = 1
 
 print( occurence )
+
+"""
 
 
 
