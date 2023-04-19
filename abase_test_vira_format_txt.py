b766a082eb566bf20c42956eb704f6dc0a49522a
IJF444 <IJF444.engie.com>
2023-04-19 11:52:02 +0200

    Update with title

--- abase_test_vira_format_txt.py
+++ abase_test_vira_format_txt.py
@@ -3,16 +3,20 @@
 import unittest
 import codecs
 
-liste = ['tomate','pomme', 'cerise', 'fraise','mirtille']
+liste = ['tomate','pomme', 'cerise', 'fraise','mirtille', 'cassis']
 
 nomFichier = 'monjardin.txt'
 file = codecs.open (nomFichier, 'w+', 'utf-8')
 
+file.write("Mon jardin\n")
+
 for i in range ( len(liste) ) : 
     monjardin = liste [i]
     #print('legume n°' + str(i) + '->' + monjardin + 'vendu au kg')
-    print( "fruit n°{idx:<3d} -> {fruit:^20s} vendu au kilo({fruit})".format( idx=i, fruit=monjardin ) )
+    print( "fruit n°{idx:<3d} ->   {fruit:<20s} vendu au kilo({fruit})".format( idx=i, fruit=monjardin ) )
+    
+  
 
-    file.writelines("fruit n°{idx:<3d} -> {fruit:<20s} vendu au kilo({fruit})\n".format( idx=i, fruit=monjardin ))
+    file.writelines("fruit n°{idx:<3d} ->   {fruit:<20s} vendu au kilo({fruit})\n".format( idx=i, fruit=monjardin ))
 
 file.close()
