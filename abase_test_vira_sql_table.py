ea95fc7a12ecd6a0b13392d12c0fe121f347e799
IJF444 <IJF444.engie.com>
2023-04-19 16:30:01 +0200

    Fill the sql table exemple

--- abase_test_vira_sql_table.py
+++ abase_test_vira_sql_table.py
@@ -1,17 +1,13 @@
 import sqlite3
 
 connexion = sqlite3.connect('ma_base_de_donnees.db')
-
 curseur = connexion.cursor()
 
-requete = """CREATE TABLE Employes (
-                id INTEGER PRIMARY KEY,
-                nom TEXT NOT NULL,
-                prenom TEXT NOT NULL,
-                date_embauche DATE NOT NULL,
-                salaire REAL NOT NULL
-            )"""
+# Inserting data
+curseur.execute("INSERT INTO Employes (id, nom, prenom, date_embauche, salaire) VALUES (1, 'Doe', 'John', '2022-01-01', 5000.00)")
+curseur.execute("INSERT INTO Employes (id, nom, prenom, date_embauche, salaire) VALUES (2, 'Smith', 'Jane', '2022-01-01', 6000.00)")
 
-curseur.execute(requete)
+connexion.commit()
 
 connexion.close()
+
