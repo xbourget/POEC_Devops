8786b1cf3bdeaec7f016d7fde0c86a7444346c5f
IJF444 <IJF444.engie.com>
2023-04-19 11:55:14 +0200

    Optimize with title

--- abase_test_vira_format_txt.py
+++ abase_test_vira_format_txt.py
@@ -14,8 +14,6 @@ for i in range ( len(liste) ) :
     monjardin = liste [i]
     #print('legume n°' + str(i) + '->' + monjardin + 'vendu au kg')
     print( "fruit n°{idx:<3d} ->   {fruit:<20s} vendu au kilo({fruit})".format( idx=i, fruit=monjardin ) )
-    
-  
 
     file.writelines("fruit n°{idx:<3d} ->   {fruit:<20s} vendu au kilo({fruit})\n".format( idx=i, fruit=monjardin ))
 
