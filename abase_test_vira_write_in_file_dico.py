785ac5331cf3b09101ce2baa6fce387bf646191f
IJF444 <IJF444.engie.com>
2023-04-19 13:40:11 +0200

    Add keys

--- abase_test_vira_write_in_file_dico.py
+++ abase_test_vira_write_in_file_dico.py
@@ -3,7 +3,7 @@ from myLib.scrabble import scrabble
 nomFichier = 'monDico.txt'
 file = open(nomFichier, 'w') # handle
 
-for letter in scrabble:
+for letter in scrabble.keys():
     point = scrabble[letter]["point"]
     nombre = scrabble[letter]["nombre"]
     print("Letter {letter:<3s} -> {point:<2d} points ({nombre:<2d} occurences)".format(letter=letter, point=point, nombre=nombre))
