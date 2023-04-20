e3d371885d8cfc306cd78f482c9889c9c24ae346
IJF444 <IJF444.engie.com>
2023-04-20 11:39:36 +0200

    Add date

--- abase_test_vira_excel.py
+++ abase_test_vira_excel.py
@@ -1,5 +1,7 @@
 #pip install panda
 import pandas as pd
+import datetime
+import os
 
 df = pd.read_excel("file_name.xlsx")
 
@@ -10,3 +12,11 @@ df.at[2, 'A'] = 'Hello'
 print(df.head(20))
 
 df.to_excel('my_excel_file.xlsx', index=False)
+
+now = datetime.datetime.now().strftime("%Y-%m-%d")
+
+file_name = "my_excel_file.xlsx"
+
+new_file_name = f"{now}_{file_name}"
+
+os.rename(file_name, new_file_name)
