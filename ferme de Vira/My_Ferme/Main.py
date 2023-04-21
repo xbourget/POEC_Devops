9366996862e53f670402bdd8ee96bbf0df6a5519
IJF444 <IJF444.engie.com>
2023-04-21 12:13:49 +0200

    update

--- ferme de Vira/My_Ferme/Main.py
+++ ferme de Vira/My_Ferme/Main.py
@@ -10,15 +10,15 @@ from mouton  import Mouton
 
 ferme = Ferme( 'du moulin' )
 
-ferme.addPature( 'du ruisseau', 3 ) #suface de la pature 3ha
+ferme.addPature( 'du ruisseau', 3 ) 
 
 pature = ferme.getPature( 'du ruisseau' )
 
 pature.add( Cheval( 'Jumpy Jumper'  ) )
 pature.add( Chien( 'Rantanplan'  ) )
-pature.add( Vache( 'marguerite'  ) )
-pature.add( Vache( 'blanchette'  ) )
-pature.add( Mouton( 'memeeee'  ) )
+pature.add( Vache( 'Milka'  ) )
+pature.add( Vache( 'Luska'  ) )
+pature.add( Mouton( 'Shawn'  ) )
 
 ferme.addRessource( 'foin', 30, 'tonnes'  )
 ferme.addRessource( 'eau',  15000, 'litres'  )
