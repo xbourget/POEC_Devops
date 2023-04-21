71ab62fdd476e6f70a4af46d785ee992a0f02b77
IJF444 <IJF444.engie.com>
2023-04-21 15:22:06 +0200

    Update actions

--- ferme_de_Vira/myfermeLib/animal.py
+++ ferme_de_Vira/myfermeLib/animal.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python3
 # -*- coding: utf-8  -*-
-
-
 from myfermeLib.ferme import Ferme
 from myfermeLib.pature import Pature
 from myfermeLib.ressource import Ressource
-
 class Animal()  :
-
     def __init__(self, nom):
         self.nom = nom
         self.energie = 10
         self.eau = 10
-
     def lever( self ):
-        print( str(self), 'je me lève' )
-
+        self.message( 'je me lève' )
     def coucher( self ):
-        print( str(self), 'je me couche' )
-
+        self.message( 'je me couche' )
     def manger( self ):
-        print( str(self), 'je mange' )
-        ressource = Ferme.getRessource( 'foin' )
-        self.energie += ressource.consommer( 10 )
-        ressource = Ferme.getRessource( 'eau' )
-        self.eau += ressource.consommer( 10 )
-
+        if self.isVivant() :
+            self.message(  'je mange' )
+            ressource = Ferme.getRessource( 'eau' )
+            self.eau += ressource.consommer( 10 )
+    def bouger( self, qteMouvement, verbe='je bouge' ):
+        if self.isVivant() :
+            self.message( verbe )
+            self.eau -= qteMouvement
+            self.energie -= qteMouvement
+    def message( self, msg ):
+        if self.isVivant() :
+            print( str(self), msg )
+        
     def __str__( self ):
-        return 'inconnu'
+        if not self.isVivant() :
+            return "{0} mort".format(self.nom )
+        else:
+            return "{0} {1}litres {2}calories".format(self.nom, self.eau, self.energie )
+    def isVivant( self ):
+        return self.eau > 0 and self.energie > 0
     
 
 
