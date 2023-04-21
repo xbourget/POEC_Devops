b05d7413a188326d41e4a251492f33699fa18e97
IJF444 <IJF444.engie.com>
2023-04-21 15:17:07 +0200

    Update animaux

--- ferme_de_Vira/myfermeLib/mouton.py
+++ ferme_de_Vira/myfermeLib/mouton.py
@@ -11,6 +11,16 @@ class Mouton( Animal )  :
 
     def __str__( self ):
         return 'Je suis Shawn le mouton ' + self.nom
+
+        if self.isVivant() :
+            super().manger()
+            ressource = Ferme.getRessource( 'foin' )
+            self.energie += ressource.consommer( 5 )
+
+    def bouger( self, qteMouvement ):
+        if self.isVivant() :
+            super().bouger( qteMouvement * 0.5, 'je broute'  )
+
     
 
 
--- ferme_de_Vira/myfermeLib/vache.py
+++ ferme_de_Vira/myfermeLib/vache.py
@@ -12,7 +12,14 @@ class Vache( Animal )  :
     def __str__( self ):
         return 'Je suis Milka, j\'habite dans les Alpes ' + self.nom
     
+        if self.isVivant() :
+            super().manger()
+            ressource = Ferme.getRessource( 'foin' )
+            self.energie += ressource.consommer( 5 )
 
+    def bouger( self, qteMouvement ):
+        if self.isVivant() :
+            super().bouger( qteMouvement * 0.5, 'je cours'  )
 
 
 
